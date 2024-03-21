87f122773fa2e567027da87be7a0d3d2541f8378
sescandon <sescandon@unal.edu.co>
2024-03-21 13:47:21 -0500

    Added gitignore and modified init so it also has commands for Windows

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 ## Create Virtual Env
 python3 -m venv venv
-source venv/bin/activate
+source venv/bin/activate # On Windows it should be .\venv\Scripts\activate
 
 ## Install required packages
 pip install fastapi 'strawberry-graphql[debug-server]' "uvicorn[standard]"
