e9524b1ddd1c629eecdc5357fb59305fbaa17f84
NicolasLopez147 <nilopezn@unal.edu.co>
2024-04-02 04:48:09 -0500

    Merge

--- app/utils.py
+++ app/utils.py
@@ -29,19 +29,5 @@ async def CheckAdmin(id:int):
         return{
             "isAdmin": isAdmin,
             "Code": response.status_code,
-        }
             "role": response.text.role,
         }
-
-# async def CheckRole(userToken: str):
-#     api_url = os.environ.get("USERS_URL")
-#     auth_url = api_url + "/Users/setup"
-#     api_url = os.environ.get("USERS_URL")
-#     auth_url = api_url + "/Users/role"
-#     async with httpx.AsyncClient() as client:
-#         response = await client.get(auth_url, params={"idUser": idUser})
-#         if response.status_code == 200:
-#             role = response.text.role
-#         else:
-#             role = None
-#         return role
