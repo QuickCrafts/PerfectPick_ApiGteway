3889d233f2b6ca2f2572c44f41cdd7be217cbedb
dmriveroe <104804272+XdmriveroeX@users.noreply.github.com>
2024-04-07 18:40:07 -0500

    mega docker

--- app/GraphQL/Recommendations/recommendationMutations.py
+++ app/GraphQL/Recommendations/recommendationMutations.py
@@ -5,7 +5,7 @@ import json
 from strawberry.types import Info
 from app.GraphQL.Recommendations.recommendationTypes import MovieRecommendation
 from app.GraphQL.Recommendations.recommendationTypes import RecommendationResponse,CreateRecommendationInput,RecommendationResponse
-
+from app.GraphQL.Recommendations.recommendationTypes import UpdateRecommendationInput
 
 async def fetch_likes(user_id: int) -> dict:
     likes_service_url = f"http://PerfectPick_Likes_ms:3000/likes/user/{user_id}"
@@ -13,14 +13,10 @@ async def fetch_likes(user_id: int) -> dict:
         response = await client.get(likes_service_url)
         if response.status_code == 200:
             data = response.json()
-            # Extract media_ids for movies
             movie_ids = [movie['media_id'] for movie in data['movies']] if data['movies'] else []
-            # Assuming similar structure for songs and books, repeat the process
-            # For songs and books, if they're supposed to be lists and not null when empty
             song_ids = [song['media_id'] for song in data['songs']] if data['songs'] else []
             book_ids = [book['media_id'] for book in data['books']] if data['books'] else []
             
-            # Construct the new response
             transformed_response = {
                 "id_user": user_id,
                 "movies": movie_ids,
@@ -30,6 +26,13 @@ async def fetch_likes(user_id: int) -> dict:
             return transformed_response
         else:
             return {}
+
+
+
+async def update_recommendation(self, user_id: str, input: UpdateRecommendationInput) -> str:
+        # Implement the logic to call the REST API to update the recommendation here.
+        ...
+        return "Recommendation updated successfully"
 
 
 '''
