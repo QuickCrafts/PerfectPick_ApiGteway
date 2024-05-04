1f7e98295faad398a40dff2db757d360428b8370
Paula Guzmán <pauladaguz@gmail.com>
2024-05-04 11:45:51 -0500

    Advance Automatic

--- run_gateway.sh
+++ run_gateway.sh
@@ -1,11 +1,17 @@
+echo "Delete existing API Gateway container"
+
 docker stop apigateway
 docker rm apigateway
 
+echo "Delete existing API Gateway image"
+
 docker rmi apigateway
+
+echo "Build image API Gateway"
 
 docker build -t apigateway .
 
 echo "running docker..."
 
-docker run -d --name apigateway --network perfectpickusersnetwork -p 9000:9000 apigateway 
+docker run -d --name apigateway --network perfectpicknetwork -p 9000:9000 apigateway 
 
